e2a2ea359cd8523efc43d38a61c9f9b537a1611d
yupyup-Ahyeon <yupyupman2001@gmail.com>
2025-03-13 19:15:12 +0900

    updated to use task_type not using isinstance

--- scenarios/collaborative_transport/agent.py
+++ scenarios/collaborative_transport/agent.py
@@ -3,7 +3,7 @@ import math
 import os
 from modules.utils import config, generate_positions 
 from modules.base_agent import BaseAgent
-from scenarios.simple.task import task_colors
+from scenarios.collaborative_transport.task import task_colors
 
 # Load agent configuration (Scenario Specific)
 work_rate = config['agents']['work_rate']
@@ -16,8 +16,13 @@ class Agent(BaseAgent):
         super().__init__(agent_id, position, tasks_info)
         self.work_rate = work_rate
 
-        
-        self.task_amount_done = 0.0        
+
+        self.task_amount_done = 0.0
+        self.target_vertex_idx = None
+        self.task_color_id = None
+
+    def set_target_vertex_idx(self, target_vertex_idx):
+        self.target_vertex_idx = target_vertex_idx
 
     def draw(self, screen):
         size = 10
@@ -31,11 +36,57 @@ class Agent(BaseAgent):
         self.update_color()
         pygame.draw.polygon(screen, self.color, [p1, p2, p3])
 
+
+    def set_color_id(self, task_color_id):
+        self.task_color_id = task_color_id
+
     def update_color(self):        
-        self.color = task_colors.get(self.assigned_task_id, (20, 20, 20))  # Default to Dark Grey if no task is assigned
+        self.color = task_colors.get(self.task_color_id, (20, 20, 20))  # Default to Dark Grey if no task is assigned
+    
+    def find_matching_slot_task(self, local_block_task, local_tasks_info):
+        slot_task_id = local_block_task.task_id + 1   # block에 '알맞은 짝의' slot이 존재하는지 id로 판단...
+        for task in local_tasks_info:
+            if task.task_id == slot_task_id and task.task_type == "slot":
+                return task
+        return None
+    
+    # base_agent.py의 get_tasks_nearby()를 수정
+    def get_all_tasks_nearby(self, radius = None, with_completed_task = True):
+        _situation_awareness_radius = self.situation_awareness_radius if radius is None else radius
+        if _situation_awareness_radius > 0:
+            situation_awareness_radius_squared = _situation_awareness_radius ** 2
+            if with_completed_task: # Default
+                local_tasks_info = [
+                    task 
+                    for task in self.tasks_info 
+                    if (self.position - task.position).length_squared() <= situation_awareness_radius_squared
+                ]                
+            else:
+                local_tasks_info = [
+                    task 
+                    for task in self.tasks_info 
+                    if not (task.completed or task.vertex_agent_num_completed) and (self.position - task.position).length_squared() <= situation_awareness_radius_squared
+                ]                                
+        else:
+            if with_completed_task: # Default
+                local_tasks_info = self.tasks_info
+            else:
+                local_tasks_info = [
+                    task 
+                    for task in self.tasks_info 
+                    if not (task.completed or task.vertex_agent_num_completed)
+                ]                                                
+        
+        return local_tasks_info  
 
+    # base_agent.py의 get_tasks_nearby()를 오버라이드
+    def get_tasks_nearby(self, radius = None, with_completed_task = True): # block task만 선택
+        local_tasks_info = self.get_all_tasks_nearby(radius, with_completed_task)
 
+        local_block_tasks_info = [task for task in local_tasks_info if task.task_type == "block"]
 
+        return local_block_tasks_info
+    
 def generate_agents(tasks_info):
     agent_quantity = config['agents']['quantity']
     agent_locations = config['agents']['locations']
